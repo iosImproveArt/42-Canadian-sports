88c803e616b78c6536dcbb0fdfeb06414d4edefb
iosImproveArt <impiosart@gmail.com>
2025-01-06 15:59:44 +0200

    init

---
+++ Canadian Sport/Canadian_SportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Canadian_SportApp.swift
+//  Canadian Sport
+//
+//  Created by Improve on 06.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Canadian_SportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
